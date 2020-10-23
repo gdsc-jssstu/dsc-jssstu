c69026ed2dda1cecb2815ab9eb62519250af175e
Poorvi <poorvishetty1202@gmail.com>
2020-10-23 14:38:58 +0530

    views: Team sidebar bug fix
    -reduced dependancy on bootstrap styling (again)

--- views/assets/js/team.js
+++ views/assets/js/team.js
@@ -6,10 +6,10 @@ function flip(id)
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById("main").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.display = "block";
 }
