4c8e10d611fa6b14856541f1d24b41b3991d4a41
Pranav Bedre <bedrepranav@gmail.com>
2020-11-01 15:05:08 +0530

    client: Use preact
    Signed-off-by: Pranav Bedre <bedrepranav@gmail.com>

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,19 @@
 const withPWA = require("next-pwa");
+const withPrefresh = require("@prefresh/next");
+const preact = require("preact");
+const withPreact = require("next-plugin-preact");
+
 const prod = process.env.NODE_ENV === "production";
 
-module.exports = withPWA({
-  pwa: {
-    disable: prod ? false : true,
-    dest: "public",
-    skipWaiting: false,
-  },
-  experimental: {
-    modern: true,
-    polyfillsOptimization: true,
-  },
-});
+module.exports = withPreact(
+  withPWA({
+    pwa: {
+      disable: prod ? false : true,
+      dest: "public",
+      skipWaiting: false,
+    },
+    experimental: {
+      modern: true,
+    },
+  })
+);
